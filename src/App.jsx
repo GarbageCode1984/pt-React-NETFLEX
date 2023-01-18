5ee31e7448958c9a9edfb66e9a26d12713695873
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-01-18 22:41:52 +0900

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/Home";
+import MovieDetail from "./pages/MovieDetail";
+import Movies from "./pages/Movies";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return (
-    <>
-    <h1>기본 템플릿용</h1>
-    </>
-  );
+    return (
+        <>
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/movies" element={<Movies />}></Route>
+                <Route path="/movies/:id" element={<MovieDetail />}></Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
